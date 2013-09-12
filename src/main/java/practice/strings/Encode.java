77ccf010ccacfca515c241eb0fe4a6449f4b4636
plawler <paul.lawler@gmail.com>
2013-09-11 23:11:25 -0400

    encode a string

---
+++ src/main/java/practice/strings/Encode.java
@@ -0,0 +1,37 @@
+package practice.strings;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: paul
+ * Date: 9/11/13
+ * Time: 10:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Encode {
+
+    public static void main(String[] args) {
+        String url = "/fo o/b ar";
+        System.out.println(encode(url));
+    }
+
+    private static String encode(String url) {
+        final char[] replacement = new char[] {'%', '2', '0'};
+        char[] origin = url.toCharArray();
+        char[] buffer = new char[origin.length * 2]; // buffer == 16
+
+        int offset = 0;
+        for (int i = 0; i < origin.length; i++) {
+            if (origin[i] == ' ') {
+                for (int j = 0; j < replacement.length; j++) {
+                    buffer[i+offset+j] = replacement[j];
+                }
+                offset += replacement.length-1;
+            } else {
+                buffer[i+offset] = origin[i];
+            }
+        }
+
+        return new String(buffer);
+    }
+
+}
