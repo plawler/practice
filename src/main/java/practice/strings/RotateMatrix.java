13a366c6dc4f31085061ebfa798daf64401b1b4a
plawler <paul.lawler@gmail.com>
2013-09-15 23:38:56 -0400

    rotate NxN matrix

---
+++ src/main/java/practice/strings/RotateMatrix.java
@@ -0,0 +1,44 @@
+package practice.strings;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: paul
+ * Date: 9/15/13
+ * Time: 10:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RotateMatrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[2][2];
+        int counter = 1;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                matrix[i][j] = counter;
+                counter++;
+            }
+
+        }
+
+        int[][] rotated = rotate(matrix);
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.println(rotated[i][j]);
+            }
+        }
+    }
+
+    public static int[][] rotate(int[][] matrix) {
+        int[][] rotated = new int[2][2];
+        for (int col = 0; col < matrix.length; col++) {
+            for (int row = matrix.length - 1; row >= 0; row--) {
+                rotated[col][row] = matrix[row][col];
+//                System.out.println(matrix[row][col]);
+            }
+        }
+        return rotated;
+    }
+
+
+}
